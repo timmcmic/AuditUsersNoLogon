01b36481646416309d746073c723915210b30b72
timmcmic <timmcmic@microsoft.com>
2023-04-25 15:12:19 -0400

    Fixing comments

--- AuditUsersNoLogon.ps1
+++ AuditUsersNoLogon.ps1
@@ -5,15 +5,15 @@ Connect-MGGraph -scopes User.Read.All,AuditLog.Read.All
 
 #(Note consent may be required if you have not utilized these graph scopes for the user being specified on logon.)
 
-$events = Get-MgAuditLogSignIn -All  (This command pulls all the events from the audit logs for the last 30 days)
+$events = Get-MgAuditLogSignIn -All  #(This command pulls all the events from the audit logs for the last 30 days)
 
-$successEvents = $events | where {$_.status.errorCode -eq 0}  (This command pulls all of the events from the audit logs for the last 30 days)
+$successEvents = $events | where {$_.status.errorCode -eq 0}  #(This command pulls all of the events from the audit logs for the last 30 days)
 
 $uniqueSuccessEvents = $successEvents | sort-object -property UserPrincipalName -unique
 
 #The final variable here contains a list of user principal names that have signed into the environment and were successful.  Your’re request is for accounts that have not signed in.
 
-$users = get-MGUser -all  (Get all of the users into a variable).
+$users = get-MGUser -all  #(Get all of the users into a variable).
 
 Foreach ($successEvent in $uniqueSuccessEvents)
 {
